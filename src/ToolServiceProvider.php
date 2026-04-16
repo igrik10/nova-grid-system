9835bba1abfb94dd3cfc3b4960b8418cd8b0df3e
Ihor <it-support@skehard-research.com>
2026-04-16 23:56:12 +0300

    fix: resolve NovaRequest lazily in Field macros to avoid early binding issues in Nova 5

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -23,9 +23,6 @@ class ToolServiceProvider extends ServiceProvider
 
         Nova::serving(function (ServingNova $event) {
 
-            /**
-             * HELPERS MACROS
-             */
             NovaRequest::macro('getRequestType', function () {
                 if ($this->isCreateOrAttachRequest()) {
                     return 'creating';
@@ -52,31 +49,34 @@ class ToolServiceProvider extends ServiceProvider
                 return config('nova-grid-system.'.$meta.'_scope.'.$requestType, true);
             });
 
-            /** @var NovaRequest $novaRequest */
-            $novaRequest = app(NovaRequest::class);
-
-            Field::macro('withSize', function (string $size = 'w-full') use ($novaRequest) {
+            // All Field macros resolve NovaRequest lazily to avoid early binding issues
+            Field::macro('withSize', function (string $size = 'w-full') {
+                /** @var NovaRequest $novaRequest */
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivateGlobally('stacked')) {
                     $this->stacked();
                 }
                 return $this->withMeta(['size' => $size]);
             });
 
-            Field::macro('size', function ($size = 'w-full') use ($novaRequest) {
+            Field::macro('size', function ($size = 'w-full') {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('size', 'all', true)) {
                     $this->withSize($size);
                 }
                 return $this;
             });
 
-            Field::macro('removeBottomBorder', function ($remove = true) use ($novaRequest) {
+            Field::macro('removeBottomBorder', function ($remove = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('remove_bottom_border', 'all', true)) {
                     $this->withMeta(['removeBottomBorder' => $remove]);
                 }
                 return $this;
             });
 
-            Field::macro('sizeOnCreating', function ($size = 'w-full') use ($novaRequest) {
+            Field::macro('sizeOnCreating', function ($size = 'w-full') {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('size', 'creating')) {
                     if (config('nova-grid-system.stacked_auto', true) && config('nova-grid-system.creating.stacked', true)) {
                         $this->stacked();
@@ -86,77 +86,88 @@ class ToolServiceProvider extends ServiceProvider
                 return $this;
             });
 
-            Field::macro('stackedOnCreating', function ($stacked = true) use ($novaRequest) {
+            Field::macro('stackedOnCreating', function ($stacked = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('stacked', 'creating')) {
                     $this->stacked($stacked);
                 }
                 return $this;
             });
 
-            Field::macro('removeBottomBorderOnCreating', function ($remove = true) use ($novaRequest) {
+            Field::macro('removeBottomBorderOnCreating', function ($remove = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('remove_bottom_border', 'creating')) {
                     $this->withMeta(['removeBottomBorder' => $remove]);
                 }
                 return $this;
             });
 
-            Field::macro('sizeOnUpdating', function ($size = 'w-full') use ($novaRequest) {
+            Field::macro('sizeOnUpdating', function ($size = 'w-full') {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('size', 'updating')) {
                     $this->withSize($size);
                 }
                 return $this;
             });
 
-            Field::macro('stackedOnUpdating', function ($stacked = true) use ($novaRequest) {
+            Field::macro('stackedOnUpdating', function ($stacked = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('stacked', 'updating')) {
                     $this->stacked($stacked);
                 }
                 return $this;
             });
 
-            Field::macro('removeBottomBorderOnUpdating', function ($remove = true) use ($novaRequest) {
+            Field::macro('removeBottomBorderOnUpdating', function ($remove = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('remove_bottom_border', 'updating')) {
                     $this->withMeta(['removeBottomBorder' => $remove]);
                 }
                 return $this;
             });
 
-            Field::macro('sizeOnForms', function ($size = 'w-full') use ($novaRequest) {
+            Field::macro('sizeOnForms', function ($size = 'w-full') {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('size', 'forms')) {
                     $this->withSize($size);
                 }
                 return $this;
             });
 
-            Field::macro('stackedOnForms', function ($stacked = true) use ($novaRequest) {
+            Field::macro('stackedOnForms', function ($stacked = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('stacked', 'forms')) {
                     $this->stacked($stacked);
                 }
                 return $this;
             });
 
-            Field::macro('removeBottomBorderOnForms', function ($remove = true) use ($novaRequest) {
+            Field::macro('removeBottomBorderOnForms', function ($remove = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('remove_bottom_border', 'forms')) {
                     $this->withMeta(['removeBottomBorder' => $remove]);
                 }
                 return $this;
             });
 
-            Field::macro('sizeOnDetail', function ($size = 'w-full') use ($novaRequest) {
+            Field::macro('sizeOnDetail', function ($size = 'w-full') {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('size', 'detail')) {
                     $this->withSize($size);
                 }
                 return $this;
             });
 
-            Field::macro('stackedOnDetail', function ($stacked = true) use ($novaRequest) {
+            Field::macro('stackedOnDetail', function ($stacked = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('stacked', 'detail')) {
                     $this->stacked($stacked);
                 }
                 return $this;
             });
 
-            Field::macro('removeBottomBorderOnDetail', function ($remove = true) use ($novaRequest) {
+            Field::macro('removeBottomBorderOnDetail', function ($remove = true) {
+                $novaRequest = app(NovaRequest::class);
                 if ($novaRequest->isActivate('remove_bottom_border', 'detail')) {
                     $this->withMeta(['removeBottomBorder' => $remove]);
                 }
